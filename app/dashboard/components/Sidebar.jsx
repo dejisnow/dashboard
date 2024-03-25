0609614b749c040a0960cba655a169d5f9c70c6c
DEJI_SNOW <deji.snow@gmail.com>
2024-03-25 17:39:26 +0100

    dashboard styling

--- app/dashboard/components/Sidebar.jsx
+++ app/dashboard/components/Sidebar.jsx
@@ -1,14 +1,25 @@
+
+import Link from 'next/link'
 import React from 'react'
+import { WiLightning } from "react-icons/wi";
+import { WiEarthquake } from "react-icons/wi";
 
-export const Sidebar = () => {
+
+export const Sidebar = ({children}) => {
   return (
-    <div>
-        <div>
-            Analytics
-        </div>
-        <div>Applications</div>
+    <div className='text-sm bg-slate-500 h-screen p-2'>
 
+        <ul className=' '>
+        <Link href={''}> <li className='my-1 hidden md:flex hover:bg-blue-300 p-1 '>Analytics</li><WiEarthquake className='md:hidden text-2xl p-1 my-1 hover:bg-blue-200 '/></Link>
+        <Link href={''}> <li className='my-1 hidden md:flex hover:bg-blue-300 p-1 '>Energy</li><WiEarthquake className='md:hidden text-2xl p-1 my-1 hover:bg-blue-200 '/></Link>
+        <Link href={''}> <li className='my-1 hidden md:flex hover:bg-blue-300 p-1 '>Transactions</li><WiEarthquake className='md:hidden text-2xl p-1 my-1 hover:bg-blue-200 '/></Link>
+        <Link href={''}> <li className='my-1 hidden md:flex hover:bg-blue-300 p-1 '>Wallet</li><WiEarthquake className='md:hidden text-2xl p-1 my-1 hover:bg-blue-200 '/></Link>
+         
+        </ul>
+   
+       
 
+      <main>{children}</main>
     </div>
   )
 }
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,11 +3,23 @@ import { Sidebar } from './components/Sidebar'
 
 export default function page() {
   return (
-    <div className='flex justify-between'>
-      <Sidebar />
+    <div className='grid grid-flow-col grid-cols-5 md:grid-cols-8 '>
+      <div className=' '>
+        <Sidebar />
 
-      <div>
-        Display
+      </div>
+      <div className=' text-sm md:text-base col-span-4 md:col-span-7'>
+        <div className=' grid grid-cols-4 h-1/4'>
+        <div>Data1</div>
+        <div className='bg-red-300 col-span-2'>Data2</div>
+        <div>Data3</div>
+        </div>
+        <div className='grid grid-cols-4 h-3/4'>
+        <div className='col-span-3 bg-blue-300'>Chart</div>
+        <div>Stat</div>
+        
+        </div>
+        
       </div>
 
     </div>
